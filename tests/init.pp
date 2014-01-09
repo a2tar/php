a50761037fbf3eb437e722e0ded318332edaad02
a2tar <a2@stealthq.com>
2014-01-09 23:56:57 +0200

    x3

--- tests/init.pp
+++ tests/init.pp
@@ -1,1 +1,17 @@
-include php
+include php::fpm
+
+php::fpm::pool { "billing": unix_socket_path => true, }
+
+php::module { [
+  "php5-curl",
+  "php5-gd",
+  "php5-mcrypt",
+  "php5-mysql",
+  "php5-sqlite",
+  "php5-suhosin",
+  "php5-xsl"]:
+}
+include php::extra::browscap
+
+php::pear::module { "Net_GeoIP": }
+
